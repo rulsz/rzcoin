8e9dc196095dc1a7c03dca31b8fe73e95496d488
RZ PEDIA <72466978+rulsz@users.noreply.github.com>
2024-09-01 22:03:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,5 @@ const tgWebAppData = url.hash.split('&')[0].split('=')[1];
 const userData = JSON.parse(decodeURIComponent(tgWebAppData));
 const username = userData.username;
 
-navigator.clipboard.writeText(username).then(() => {
-  console.log('Username copied to clipboard!');
-}).catch((error) => {
-  console.error('Error copying username:', error);
-});
+const infoElement = document.querySelector('.info');
+infoElement.innerHTML = currentUrl;
