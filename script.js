e264d29fa50be3d5bc8cf1a72295e58e7eebb58e
RZ PEDIA <72466978+rulsz@users.noreply.github.com>
2024-09-01 20:23:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot('7507093707:AAH2WtcKPHJ_o-7UjwurMnwL96gs7BKTXFU', { polling: true });
+const currentUrl = window.location.href;
+const url = new URL(currentUrl);
+const tgWebAppData = url.hash.split('&')[0].split('=')[1];
+const userData = JSON.parse(decodeURIComponent(tgWebAppData));
+const username = userData.username;
 
-bot.getChat('@rulfdyy', (err, chat) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const chatId = chat.id;
-    console.log(`Chat ID: ${chatId}`);
-    // You can now use the chat ID to send messages to the user
-    bot.sendMessage(chatId, 'Hello!');
-  }
+navigator.clipboard.writeText(username).then(() => {
+  console.log('Username copied to clipboard!');
+}).catch((error) => {
+  console.error('Error copying username:', error);
 });
