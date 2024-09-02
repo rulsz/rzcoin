3432fff73f08c627cadbe04b46e5cfc398867c5e
RZ PEDIA <72466978+rulsz@users.noreply.github.com>
2024-09-02 18:04:25 +0700

    Update sakura.js

--- JS/sakura.js
+++ JS/sakura.js
@@ -1,3 +1,4 @@
+document.body.style.backgroundImage = 'url(img/sakura_background.png)';
 // Mendapatkan elemen canvas
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
