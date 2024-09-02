6f1cd8bb3a7f7b9f3b94c3dcb35d9da6c632f484
RZ PEDIA <72466978+rulsz@users.noreply.github.com>
2024-09-02 16:44:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-const currentUrl = window.location.href;
-const url = new URL(currentUrl);
-const tgWebAppData = url.hash.split('&')[0].split('=')[1];
-const userData = JSON.parse(decodeURIComponent(tgWebAppData));
-const username = userData.username;
+let progress = 0;
+let intervalId;
+let isProgressRunning = false;
+let getClaim = false;
 
-const infoElement = document.querySelector('.info');
-infoElement.innerHTML = currentUrl;
+function startProgres() {
+  isProgressRunning = true;
+  document.querySelector('.button-farming').classList.add('active');
+
+  intervalId = setInterval(() => {
+    if (progress < 100) {
+      progress++;
+      document.querySelector('.progres-bar-fill').style.width = `${progress}%`;
+      document.querySelector('.progres-text').textContent = `Farming ${progress}%`;
+    } else {
+      clearInterval(intervalId);
+      onProgressComplete(); // Call onProgressComplete() when progress reaches 100%
+    }
+  }, 1000);
+}
+
+function onProgressComplete() {
+  let getClaim = true;
+  isProgressRunning = false;
+  document.querySelector('.button-farming').classList.remove('active');
+  document.querySelector('.button-farming').disabled = false; // enable the button again
+  document.querySelector('.progres-text').textContent = 'Claim';
+  
+}
+
+document.querySelector('.button-farming').addEventListener('click', function(event) {
+  if (isProgressRunning) {
+    console.log('Button clicked while progress is running!');
+    this.disabled = true; // disable the button
+  } else {
+    startProgres();
+  }
+});
